6babcd13486d76a02566045a0932ed2bacc7af96
Eduardo Martínez <eduardomech@gmail.com>
2019-01-01 21:55:19 +0100

    Add permissions managements for actions of initiatives types in admin panel

---
+++ decidim-initiatives/app/controllers/decidim/initiatives/admin/initiatives_types_permissions_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Initiatives
+    module Admin
+      # Controller that allows managing initiatives types
+      # permissions in the admin panel.
+      class InitiativesTypesPermissionsController < Decidim::Admin::ResourcePermissionsController
+      end
+    end
+  end
+end
